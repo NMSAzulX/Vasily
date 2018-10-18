a75317957234240e696c581ac9eea51cf86dfd9f
Hu <16236903+NMSAzulX@users.noreply.github.com>
2018-10-18 14:58:03 +0800

    Update Vasily/Extensions/SqlTypeExtentsions/DateTimeExtension.cs
    增加时间戳扩展

---
+++ Vasily/Extensions/SqlTypeExtentsions/DateTimeExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace System
+{
+    public static class DateTimeExtentsion
+    {
+        public static long MillisecondsStamp(this DateTime datetime)
+        {
+            return new DateTimeOffset(datetime).ToUnixTimeMilliseconds();
+        }
+        public static long SecondsStamp(this DateTime datetime)
+        {
+            return new DateTimeOffset(datetime).ToUnixTimeSeconds();
+        }
+        public static DateTime SecondsToTime(this long datetime)
+        {
+            DateTime dtStart = new DateTime(1970, 1, 1);
+            return dtStart.AddSeconds(datetime);
+        }
+        public static DateTime MillisecondsToTime(this long datetime)
+        {
+            DateTime dtStart = new DateTime(1970, 1, 1);
+            return dtStart.AddMilliseconds(datetime).ToLocalTime();
+        }
+    }
+}
