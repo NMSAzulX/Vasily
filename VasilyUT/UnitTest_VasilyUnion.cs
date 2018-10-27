64c31008f7f847b698d0aca14b68f45ec683e2da
Hu <16236903+NMSAzulX@users.noreply.github.com>
2018-10-27 16:43:48 +0800

    增加雪花算法，生成UniqueId

--- VasilyUT/UnitTest_VasilyUnion.cs
+++ VasilyUT/UnitTest_VasilyUnion.cs
@@ -14,7 +14,7 @@ namespace VasilyUT
         {
             SqlPackage<Student> package = new SqlPackage<Student>();
             SqlCondition<Student> condition = new SqlCondition<Student>();
-            var result = SqlUnion<Student>.Union(Sql<Student>.SelectAllByCondition + (condition > "Sid").GetConditionWithPage(), "table1", "table2", "table3");
+            var result = SqlUnion<Student>.Union(Sql<Student>.SelectAllByCondition + (condition > "Sid").Full, "table1", "table2", "table3");
             Assert.Equal("SELECT * FROM `table1` WHERE Sid > @Sid UNION SELECT * FROM `table2` WHERE Sid > @Sid UNION SELECT * FROM `table3` WHERE Sid > @Sid", result);
          }
         [Fact(DisplayName = "条件+联合语句测试2")]
@@ -22,7 +22,7 @@ namespace VasilyUT
         {
             SqlPackage<Student> package = new SqlPackage<Student>();
             SqlCondition<Student> condition = new SqlCondition<Student>();
-            var result = SqlUnion<Student>.Union(Sql<Student>.SelectAllByCondition + (condition > "Sid").GetConditionWithPage());
+            var result = SqlUnion<Student>.Union(Sql<Student>.SelectAllByCondition + (condition > "Sid").Full);
             Assert.Equal("SELECT * FROM `1` WHERE Sid > @Sid", result);
         }
     }
---
+++ VasilyUT/UnitTest_VasilyUniqueId.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VasilyUT.Entity;
+using Xunit;
+
+namespace VasilyUT
+{
+    public class UnitTest_VasilyUniqueId
+    {
+        [Fact(DisplayName = "一打雪花，带你勇闯天涯")]
+        public void TestId()
+        {
+            HashSet<long> hashSet = new HashSet<long>();
+            Snowflake<Student>.SetNodesInfo(54321, 12345);
+            for (int i = 0; i < 1000; i+=1)
+            {
+                long result = Snowflake<Student>.NextId;
+                Assert.DoesNotContain(result, hashSet);
+                hashSet.Add(result);
+            }
+        }
+    }
+}
