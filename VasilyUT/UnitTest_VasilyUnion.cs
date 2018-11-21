145d36927923c542e932f3609bf274243a142b50
Hu <16236903+NMSAzulX@users.noreply.github.com>
2018-11-21 14:56:05 +0800

    增加集合操作UT测试

--- VasilyUT/UnitTest_VasilyUnion.cs
+++ VasilyUT/UnitTest_VasilyUnion.cs
@@ -26,6 +26,14 @@ namespace VasilyUT
             var result = SqlCollection<Student>.Union(Sql<Student>.SelectAllByCondition + (condition > "Sid").Full);
             Assert.Equal("(SELECT * FROM `1` WHERE Sid > @Sid)", result);
         }
+        [Fact(DisplayName = "条件+联合语句测试3")]
+        public void TestUnionCondition3()
+        {
+            SqlPackage<Student> package = new SqlPackage<Student>();
+            SqlCondition<Student> condition = new SqlCondition<Student>();
+            var result = SqlCollection<Student>.TableIntersect(Sql<Student>.SelectAllByCondition + (condition > "Sid").Full, "table1", "table2", "table3");
+            Assert.Equal("(SELECT * FROM `table1` WHERE Sid > @Sid) INTERSECT (SELECT * FROM `table2` WHERE Sid > @Sid) INTERSECT (SELECT * FROM `table3` WHERE Sid > @Sid)", result);
+        }
     }
 
 }
