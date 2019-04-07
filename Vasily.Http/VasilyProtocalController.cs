6839c006981c1a5393d65a7bd3a9aa5e4d02edd0
Hu <16236903+NMSAzulX@users.noreply.github.com>
2019-04-07 13:37:11 +0800

    协议拆分
    根据数据的敏感度继承相应的controller

--- Vasily.Http/VasilyProtocalController.cs
+++ Vasily.Http/VasilyProtocalController.cs
@@ -2,16 +2,16 @@
 
 namespace Microsoft.AspNetCore.Mvc
 {
-    public class VasilyProtocalController<T> : VasilyController<T> where T : class
+    public class VPReadController<T> : VasilyController<T> where T : class
     {
-        public VasilyProtocalController()
+        public VPReadController()
         {
         }
-        public VasilyProtocalController(string key) : base(key)
+        public VPReadController(string key) : base(key)
         {
 
         }
-        public VasilyProtocalController(string reader, string writter) : base(reader, writter)
+        public VPReadController(string reader, string writter) : base(reader, writter)
         {
 
         }
@@ -43,6 +43,31 @@ namespace Microsoft.AspNetCore.Mvc
         {
             return Result(SqlLink<T>.Load(driver).Fields(vp.Fields).Conditions(vp).Gets<object>(vp.Instance), driver.CountWithCondition(vp));
         }
+        [HttpDelete("accurate-repeate")]
+        public ReturnResult VasilyLinkRepeate(VasilyProtocal<T> vp)
+        {
+            return Result(SqlLink<T>.Load(driver).Fields(vp.Fields).Conditions(vp).Repeate(vp.Instance));
+        }
+        #endregion
+    }
+
+    public class VPWriteController<T> : VasilyController<T> where T : class
+    {
+        public VPWriteController()
+        {
+        }
+        public VPWriteController(string key) : base(key)
+        {
+
+        }
+        public VPWriteController(string reader, string writter) : base(reader, writter)
+        {
+
+        }
+     
+
+        #region LinkQuery
+        
         [HttpPost("accurate-modify")]
         public ReturnResult VasilyLinkModify(VasilyProtocal<T> vp)
         {
@@ -58,13 +83,90 @@ namespace Microsoft.AspNetCore.Mvc
         {
             return Result(SqlLink<T>.Load(driver).Fields(vp.Fields).Conditions(vp).Delete(vp.Instance));
         }
+        #endregion
+
+
+        #region 整个实体进行操作
+        [HttpPost("all-modify")]
+        public ReturnResult VasilyModify(params T[] instances)
+        {
+            return ModifyResult(instances);
+        }
+        [HttpPut("all-add")]
+        public ReturnResult VasilyAdd(params T[] instances)
+        {
+            return AddResult(instances);
+        }
+        [HttpDelete("all-delete")]
+        public ReturnResult VasilyDelete(params T[] instances)
+        {
+            return DeleteResult(instances);
+        }
+        #endregion
+    }
+
+    public class VPReadAndWriteController<T> : VasilyController<T> where T : class
+    {
+        public VPReadAndWriteController()
+        {
+        }
+        public VPReadAndWriteController(string key) : base(key)
+        {
+
+        }
+        public VPReadAndWriteController(string reader, string writter) : base(reader, writter)
+        {
+
+        }
+        [HttpPost("query-page-vp")]
+        public ReturnPageResult QueryPageVP(VasilyProtocal<T> vp)
+        {
+            return GetsPageResult(vp);
+        }
+
+        [HttpPost("query-vp")]
+        public ReturnResult QueryVP(VasilyProtocal<T> vp)
+        {
+            return GetResult(vp);
+        }
+
+        #region LinkQuery
+        [HttpPost("accurate-get")]
+        public ReturnResult VasilyLinkGet(VasilyProtocal<T> vp)
+        {
+            return Result(SqlLink<T>.Load(driver).Fields(vp.Fields).Conditions(vp).Get<object>(vp.Instance));
+        }
+        [HttpPost("accurate-gets")]
+        public ReturnResult VasilyLinkGets(VasilyProtocal<T> vp)
+        {
+            return Result(SqlLink<T>.Load(driver).Fields(vp.Fields).Conditions(vp).Gets<object>(vp.Instance));
+        }
+        [HttpPost("accurate-page-gets")]
+        public ReturnPageResult VasilyLinkPageGets(VasilyProtocal<T> vp)
+        {
+            return Result(SqlLink<T>.Load(driver).Fields(vp.Fields).Conditions(vp).Gets<object>(vp.Instance), driver.CountWithCondition(vp));
+        }
         [HttpDelete("accurate-repeate")]
         public ReturnResult VasilyLinkRepeate(VasilyProtocal<T> vp)
         {
             return Result(SqlLink<T>.Load(driver).Fields(vp.Fields).Conditions(vp).Repeate(vp.Instance));
         }
+        [HttpPost("accurate-modify")]
+        public ReturnResult VasilyLinkModify(VasilyProtocal<T> vp)
+        {
+            return Result(SqlLink<T>.Load(driver).Fields(vp.Fields).Conditions(vp).Modify(vp.Instance));
+        }
+        [HttpPut("accurate-add")]
+        public ReturnResult VasilyLinkAdd(VasilyProtocal<T> vp)
+        {
+            return Result(SqlLink<T>.Load(driver).Fields(vp.Fields).Conditions(vp).Add(vp.Instance));
+        }
+        [HttpDelete("accurate-delete")]
+        public ReturnResult VasilyLinkDelete(VasilyProtocal<T> vp)
+        {
+            return Result(SqlLink<T>.Load(driver).Fields(vp.Fields).Conditions(vp).Delete(vp.Instance));
+        }
         #endregion
-
 
         #region 整个实体进行操作
         [HttpPost("all-modify")]
